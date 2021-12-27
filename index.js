ee6d2b0ccc40053ff2d23af751bdb6015bda0426
frank <frankparedesalpi@gmail.com>
2021-12-27 09:42:50 -0500

    commit 1

--- index.js
+++ index.js
@@ -4,27 +4,40 @@ function calcularCuota(monto, numeroDeCuotas, interes) {
   cuota = Math.floor(cuota * 10) / 10;
   return cuota;
 }
-function digitoESC(texto) {
-  return texto.toLowerCase() == "esc";
+
+function esPositivo(num) {
+  return num >= 0;
 }
+
 function digitoSi(text) {
   return text.toLowerCase() == "si";
 }
 
-while (true) {
+let monto;
+let numeroDeCuotas;
+let interes;
+
+while (interes != 0) {
   alert("Calcular cuotas");
 
-  let monto = prompt("Digite el monto");
-  if (digitoESC(monto)) break;
+  monto = +prompt("Digite el monto");
+  numeroDeCuotas = +prompt("Digite el numero de cuotas");
+  interes = +prompt("Digite el interes");
 
-  let numeroDeCuotas = prompt("Digite el numero de cuotas");
-  if (digitoESC(numeroDeCuotas)) break;
+  if (
+    !esPositivo(monto) ||
+    !esPositivo(numeroDeCuotas) ||
+    !esPositivo(interes)
+  ) {
+    alert("Los datos de entrada no pueden ser negativos");
+    break;
+  }
 
-  let interes = prompt("Digite el interes");
-  if (digitoESC(interes)) break;
-
-  alert(calcularCuota(+monto, +numeroDeCuotas, +interes));
+  alert(calcularCuota(monto, numeroDeCuotas, interes));
 
   let salir = prompt("Desea salir");
   if (digitoSi(salir)) break;
+}
+if (interes == 0) {
+  alert("El interest no puede ser 0");
 }
